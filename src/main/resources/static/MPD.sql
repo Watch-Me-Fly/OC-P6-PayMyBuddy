e1c4c89383986e23b8fa89a8447eb8518ce17b06
watch-me-fly <thi.phoned@gmail.com>
2025-02-24 08:17:58 +0100

    Created MPD & updated README.md

---
+++ src/main/resources/static/MPD.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE PayMyBuddy;
+
+CREATE TABLE users (
+                       id INT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(255) NOT NULL UNIQUE,
+                       email VARCHAR(255) NOT NULL UNIQUE,
+                       password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE transaction (
+                             id INT AUTO_INCREMENT PRIMARY KEY,
+                             sender_id INT NOT NULL,
+                             receiver_id INT NOT NULL,
+                             description TEXT NULL,
+                             amount DECIMAL(10,2) NOT NULL,
+                             FOREIGN KEY (sender_id) REFERENCES user(id),
+                             FOREIGN KEY (receiver_id) REFERENCES user(id)
+);
+
+CREATE TABLE user_connections (
+                                  user_id INT NOT NULL,
+                                  connection_id INT NOT NULL,
+                                  PRIMARY KEY (user_id, connection_id),
+                                  FOREIGN KEY (user_id) REFERENCES user(id),
+                                  FOREIGN KEY (connection_id) REFERENCES user(id)
+);
